ee545cc990c454a037c676d26bd6d9e84207a6d0
Quinten van Til <qvantil@ambulantezorgoost.nl>
2019-09-09 15:03:06 +0200

    Add project files.

---
+++ Taxikosten 2/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Taxikosten_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.TimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.TimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.DatePicker = new System.Windows.Forms.DateTimePicker();
+            this.Kilometers = new System.Windows.Forms.TextBox();
+            this.Prijs = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TimePicker2
+            // 
+            this.TimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.TimePicker2.Location = new System.Drawing.Point(449, 249);
+            this.TimePicker2.Name = "TimePicker2";
+            this.TimePicker2.ShowUpDown = true;
+            this.TimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.TimePicker2.TabIndex = 1;
+            this.TimePicker2.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            this.TimePicker2.ValueChanged += new System.EventHandler(this.DateTimePicker2_ValueChanged);
+            // 
+            // TimePicker1
+            // 
+            this.TimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.TimePicker1.Location = new System.Drawing.Point(449, 223);
+            this.TimePicker1.Name = "TimePicker1";
+            this.TimePicker1.ShowUpDown = true;
+            this.TimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.TimePicker1.TabIndex = 2;
+            this.TimePicker1.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            this.TimePicker1.ValueChanged += new System.EventHandler(this.TimePicker1_ValueChanged);
+            // 
+            // DatePicker
+            // 
+            this.DatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.DatePicker.Location = new System.Drawing.Point(449, 275);
+            this.DatePicker.Name = "DatePicker";
+            this.DatePicker.Size = new System.Drawing.Size(200, 20);
+            this.DatePicker.TabIndex = 3;
+            this.DatePicker.Value = new System.DateTime(2019, 1, 1, 0, 0, 0, 0);
+            this.DatePicker.ValueChanged += new System.EventHandler(this.DatePicker_ValueChanged);
+            // 
+            // Kilometers
+            // 
+            this.Kilometers.Location = new System.Drawing.Point(449, 197);
+            this.Kilometers.Name = "Kilometers";
+            this.Kilometers.Size = new System.Drawing.Size(200, 20);
+            this.Kilometers.TabIndex = 4;
+            this.Kilometers.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // Prijs
+            // 
+            this.Prijs.BackColor = System.Drawing.SystemColors.Window;
+            this.Prijs.Location = new System.Drawing.Point(449, 321);
+            this.Prijs.Name = "Prijs";
+            this.Prijs.ReadOnly = true;
+            this.Prijs.Size = new System.Drawing.Size(200, 20);
+            this.Prijs.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(284, 197);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Aantal gereden kilometers:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 223);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Begin tijd:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(284, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Eind tijd:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(284, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Datum:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(284, 321);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Totale kosten rit:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Prijs);
+            this.Controls.Add(this.Kilometers);
+            this.Controls.Add(this.DatePicker);
+            this.Controls.Add(this.TimePicker1);
+            this.Controls.Add(this.TimePicker2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.DateTimePicker TimePicker2;
+        private System.Windows.Forms.DateTimePicker TimePicker1;
+        private System.Windows.Forms.DateTimePicker DatePicker;
+        private System.Windows.Forms.TextBox Kilometers;
+        private System.Windows.Forms.TextBox Prijs;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Taxikosten 2/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Taxikosten_2
+{
+    public partial class Form1 : Form
+    {
+        public int Afstand = 0;
+        public DateTime Begin;
+        public DateTime Eind;
+        public int Datum;
+        public Double Kosten = 0;
+        Double verschil = 0;
+        bool toeslag = false;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+
+
+        private void Tijden()
+        {
+            TimeSpan start = new TimeSpan(8, 0, 0);
+            TimeSpan end = new TimeSpan(18, 0, 0);
+            Double verschil1 = 0;
+            Double verschil2 = 0;
+
+            Kosten = toeslag ? Kosten / 1.15 : Kosten;
+            Kosten -= verschil;
+            verschil = 0;
+
+            // voor 08:00
+            if (Eind.TimeOfDay < start)
+            {
+                verschil = ((Eind.TimeOfDay - Begin.TimeOfDay).TotalMinutes) * 0.45;
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+
+            // na 18:00
+            else if (Begin.TimeOfDay > end)
+            {
+                verschil = ((Eind.TimeOfDay - Begin.TimeOfDay).TotalMinutes) * 0.45;
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+
+            // tussen 08:00 - 18:00
+            else if (Begin.TimeOfDay > start && Eind.TimeOfDay < end)
+            {
+                verschil = ((Eind.TimeOfDay - Begin.TimeOfDay).TotalMinutes) * 0.25;
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+
+            // Begin voor 08:00 en Eind voor 18:00
+            else if (Eind.TimeOfDay < end)
+            {
+                verschil1 = ((start - Begin.TimeOfDay).TotalMinutes) * 0.45;
+                verschil2 = ((Eind.TimeOfDay - start).TotalMinutes) * 0.25;
+                verschil = verschil1 + verschil2;
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+
+            // Begin na 08:00 en Eind na 18:00
+            else if (Begin.TimeOfDay < start)
+            {
+                verschil1 = ((end - Begin.TimeOfDay).TotalMinutes) * 0.25;
+                verschil2 = ((Eind.TimeOfDay - end).TotalMinutes) * 0.45;
+                verschil = verschil1 + verschil2;
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+
+            // Begin voor 08:00 en Eind na 18:00
+            else
+            {
+                verschil1 = ((start - Begin.TimeOfDay).TotalMinutes) * 0.25;
+                verschil2 = ((Eind.TimeOfDay - end).TotalMinutes) * 0.45;
+                verschil = verschil1 + verschil2 + ((10 * 60) * 0.25);
+                verschil = verschil < 0 ? 0 : verschil;
+
+                Kosten += verschil;
+            }
+            Prijs.Text = Kosten.ToString("C2");
+        }
+
+        private void weekendtoeslag()
+        {
+            TimeSpan start = new TimeSpan(22, 0, 0);
+            TimeSpan end = new TimeSpan(7, 0, 0);
+            Datum = (int)(DatePicker.Value.DayOfWeek);
+
+            if (Datum == 5 && Begin.TimeOfDay > start || Datum == 6 || Datum == 7 || Datum == 1 && Begin.TimeOfDay < end)
+            {
+                if (Afstand != 0 && verschil != 0)
+                {
+                    toeslag = true;
+                    Kosten *= 1.15;
+                    Prijs.Text = Kosten.ToString("C2");
+                }
+                else
+                {
+                    toeslag = false;
+                }
+            }
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            Kosten = toeslag ? Kosten / 1.15 : Kosten;
+            Kosten -= Afstand;
+            int.TryParse(Kilometers.Text, out Afstand);
+            Kosten += Afstand;
+            weekendtoeslag();
+            Prijs.Text = Kosten.ToString("C2");
+        }
+
+        private void TimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            Begin = TimePicker1.Value;
+            Tijden();
+            weekendtoeslag();
+        }
+
+        private void DateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            Eind = TimePicker2.Value;
+            Tijden();
+            weekendtoeslag();
+        }
+
+        private void DatePicker_ValueChanged(object sender, EventArgs e)
+        {
+            weekendtoeslag();
+        }
+
+
+    }
+}
